e1b53563e8b38c637a042da5c896eda2dfb41074
Lisa Westlund <lw222gu@student.lnu.se>
2014-09-17 21:24:47 +0200

    Uppdatering
    Början på felhantering.

--- 1dv402.S1.L01A/1dv402.S1.L01A/Program.cs
+++ 1dv402.S1.L01A/1dv402.S1.L01A/Program.cs
@@ -16,29 +16,47 @@ namespace _1dv402.S1.L01A
             double roundingOffAmount = 0;
             int change = 0;
 
-            // Ange totalsumma
+            // Ange totalsumma (om den är <= 0,49 ska det felhanteras)
 
-            Console.Write("Ange totalsumma: ");
-            subtotal = double.Parse(Console.ReadLine());
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Ange totalsumma\t\t: ");
+                    subtotal = double.Parse(Console.ReadLine());
+                    if (subtotal < 0.5)
+                    {
+                        throw new SystemException();
+                    }
+                    break;
+                }
 
-            //while (true)
-            //{ 
-            //    try
-            //    {
-                    
-            //        sum = double.Parse(sumText);
-            //    }
-
-            //    catch (FormatException)
-            //    {
-            //        Console.WriteLine("Du får enbart ange totalsumman i siffror. Ange totalsumman på nytt: ");
-            //    }
-            //}
+                catch (FormatException)
+                {
+                    Console.WriteLine("Du måste ange totalsumman i siffror");
+                }
+            } 
 
             // Ange erhållet belopp
 
-            Console.Write("Ange erhållet belopp: ");
-            amountReceived = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Ange erhållet belopp\t: ");
+                    amountReceived = int.Parse(Console.ReadLine());
+                    if (amountReceived < subtotal)
+                    {
+                        throw new SystemException();
+                    }
+                    break;
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("Du måste ange ett heltal.");
+                }
+            }
 
             // Beräkningar
 
@@ -49,13 +67,13 @@ namespace _1dv402.S1.L01A
             // Kvitto
 
             Console.WriteLine("\nKVITTO");
-            Console.WriteLine("-------------------------");
-            Console.WriteLine("Totalt: {0:c2}", subtotal);
-            Console.WriteLine("Öresavrundning: {0:c2}", roundingOffAmount);
-            Console.WriteLine("Att betala: {0:c0}", total);
-            Console.WriteLine("Kontant: {0:c0}", amountReceived);
-            Console.WriteLine("Tillbaka: {0:c0}", change);
-            Console.WriteLine("-------------------------");
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Totalt\t\t: {0:c2}", subtotal);
+            Console.WriteLine("Öresavrundning\t: {0:c2}", roundingOffAmount);
+            Console.WriteLine("Att betala\t: {0:c0}", total);
+            Console.WriteLine("Kontant\t\t: {0:c0}", amountReceived);
+            Console.WriteLine("Tillbaka\t: {0:c0}", change);
+            Console.WriteLine("--------------------------");
 
             // Lämna åter av respektive valuta
 
@@ -68,24 +86,47 @@ namespace _1dv402.S1.L01A
             int oneCrowns = 0;
 
             fivehundreds = change / 500;
-            onehundreds = (change % 500) / 100;
-            fiftyKronors = (change % 100) / 50;
-            twentyKronors = (change % 50) / 20;
-            tenCrowns = (change % 20) / 10;
-            fiveCrowns = (change % 10) / 5;
-            oneCrowns = (change % 5);
+            onehundreds = (change - fivehundreds * 500) % 500 / 100;
+            fiftyKronors = (change - fivehundreds * 500 - onehundreds * 100) % 100 / 50;
+            twentyKronors = (change - fivehundreds * 500 - onehundreds * 100 - fiftyKronors * 50) % 50 / 20;
+            tenCrowns = (change - fivehundreds * 500 - onehundreds * 100 - fiftyKronors * 50 - twentyKronors * 20) % 20 / 10;
+            fiveCrowns = (change - fivehundreds * 500 - onehundreds * 100 - fiftyKronors * 50 - twentyKronors * 20 - tenCrowns * 10) % 10 / 5;
+            oneCrowns = (change - fivehundreds * 500 - onehundreds * 100 - fiftyKronors * 50 - twentyKronors * 20 - tenCrowns * 10 - fiveCrowns * 5) % 5;
 
-            if (change >= 1)
+            if (fivehundreds >= 1)
             {
-                    Console.WriteLine("500-lappar: {0}", fivehundreds);
-                    Console.WriteLine("100-lappar: {0}", onehundreds);
-                    Console.WriteLine("50-lappar: {0}", fiftyKronors);
-                    Console.WriteLine("20-lappar: {0}", twentyKronors);
-                    Console.WriteLine("10-kronor: {0}", tenCrowns);
-                    Console.WriteLine("5-kronor: {0}", fiveCrowns);
-                    Console.WriteLine("1-kronor: {0}", oneCrowns);
+                Console.WriteLine("500-lappar\t: {0}", fivehundreds);
             }
 
+            if (onehundreds >= 1)
+            {
+                Console.WriteLine("100-lappar\t: {0}", onehundreds);
+            }
+
+            if (fiftyKronors >= 1)
+            {
+                Console.WriteLine("50-lappar\t: {0}", fiftyKronors);
+            }
+
+            if (twentyKronors >= 1)
+            {
+                Console.WriteLine("20-lappar\t: {0}", twentyKronors);
+            }
+
+            if (tenCrowns >= 1)
+            {
+                Console.WriteLine("10-kronor\t: {0}", tenCrowns);
+            }
+
+            if (fiveCrowns >= 1)
+            {
+                Console.WriteLine("5-kronor\t: {0}", fiveCrowns);
+            }
+
+            if (oneCrowns >= 1)
+            {
+                Console.WriteLine("1-kronor\t: {0}", oneCrowns);
+            }
         }
     }
 }
